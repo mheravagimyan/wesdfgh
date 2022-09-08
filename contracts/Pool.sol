5c22a1e9574094f8f3d1d1175f075251a2e2040c
mher <mmher0091@gmail.com>
2022-09-08 11:14:40 +0400

    ssh

--- contracts/Pool.sol
+++ contracts/Pool.sol
@@ -2,50 +2,147 @@
 pragma solidity 0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "./PoolToken.sol";
-
-contract Pool {
+import "./interfaces/IRouter.sol";
+contract Pool is Ownable{
 
     uint256 contractTokenBalance;
     IERC20 public stable;
     PoolToken public pToken;
+    uint256 profitFromArbitrage; // to know how much income after arbitrage 
+    // address owner;
 
     constructor(address _stable, address _pToken) {
         stable = IERC20(_stable);
         pToken = PoolToken(_pToken);
+        // owner = msg.sender;
     }
 
-    function deposit(uint256 amount, address tokenAddress) external payable {
-        require(amount >= 100 && amount <= 1000 ether, "Amount not in range");
-        require(tokenAddress == address(stable), "Only stable");
-        require(stable.balanceOf(msg.sender) >= amount, "Not enough balance");
+    function deposit(uint256 _amount, address _tokenAddress) external {
         require(
-            stable.allowance(msg.sender, address(this)) >= amount,
+            _amount >= 100 && _amount <= 1000 ether,
+            "Amount not in range"
+        );
+        require(
+            _tokenAddress == address(stable),
+            "Only stable"
+        );
+        require(
+            stable.balanceOf(msg.sender) >= _amount,
+            "Not enough balance"
+        );
+        require(
+            stable.allowance(msg.sender, address(this)) >= _amount,
             "Not enough allowance"
         );
 
-        uint256 mintAmount;
-
-        mintAmount = (stable.balanceOf(address(this)) == 0) ? amount / 10 : contractTokenBalance * amount / stable.balanceOf(address(this));
+        uint256 mintAmount = stable.balanceOf(address(this)) == 0 ? _amount / 10 : contractTokenBalance * _amount / stable.balanceOf(address(this));
         
-        stable.transferFrom(msg.sender, address(this), amount);
+        stable.transferFrom(msg.sender, address(this), _amount);
         contractTokenBalance += mintAmount;
         pToken.mint(msg.sender, mintAmount);
         
+    }
+
+    function withdraw(uint256 _amount) external {
+        uint256 pTokenFromAmount = _amount * contractTokenBalance / stable.balanceOf(address(this));
+
+        require(
+            pToken.balanceOf(msg.sender) >= pTokenFromAmount,
+            "Pool: Dont have enough pTokens!"
+        );
         
+        pToken.burn(msg.sender, pTokenFromAmount);
+        contractTokenBalance -= pTokenFromAmount;
+        stable.transfer(msg.sender, _amount);
     }
 
-    function foo() external{
-        stable.mint(address(this), stable.balanceOf(address(this)) / 10);
+    function arbitrage(
+        address router1,
+        address router2,
+        address token1,
+        address token2,
+        uint256 amount
+    ) external onlyOwner{
+        // uniswap router address - 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D
+        address[] memory tokens = new address[](2);
+        tokens[0] = token1;
+        tokens[1] = token2;
+        uint256 token1AmountBefore = IERC20(token1).balanceOf(address(this));
+        uint256 token2AmountBefore = IERC20(token2).balanceOf(address(this));
+
+        IRouter(router1).swapExactTokensForTokens(
+            amount,
+            1,
+            tokens,
+            address(this),
+            block.timestamp + 300
+        );
+
+        uint256 token2ProfitAfter = IERC20(token2).balanceOf(address(this)) - token2AmountBefore;
+        tokens[0] = token2;
+        tokens[1] = token1;
+
+        IRouter(router2).swapExactTokensForTokens(
+            token2ProfitAfter,
+            1,
+            tokens,
+            address(this),
+            block.timestamp + 300
+        );
+
+        uint256 token1AmountAfter = IERC20(token1).balanceOf(address(this));
+
+        require(token1AmountAfter > token1AmountBefore, "Arbitrage: Unprofitable trade!");
+        profitFromArbitrage = token1AmountAfter - token1AmountBefore;
     }
 
-    function withdraw() external {
-        require(pToken.balanceOf(msg.sender) > 0, "Pool: Dont have pTokens");
-        uint256 pTokenBalance = pToken.balanceOf(msg.sender);
-        // pTokenAmount -= pTokenBalance / 10;
+    // function arbitrage(
+    //     address router1,
+    //     address router2,
+    //     address token1,
+    //     address token2,
+    //     uint256 amount
+    // ) external onlyOwner{
+    //     // uniswap router address - 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D
+    //     address[] memory tokens = new address[](2);
+    //     tokens[0] = token1;
+    //     tokens[1] = token2;
+    //     uint256 token1AmountBefore = IERC20(token1).balanceOf(address(this));
+    //     uint256 token2AmountBefore = IERC20(token2).balanceOf(msg.sender);
 
-        pToken.burn(msg.sender, pTokenBalance);
+    //     IRouter(router1).swapExactTokensForTokens(
+    //         amount,
+    //         1,
+    //         tokens,
+    //         msg.sender,
+    //         block.timestamp + 300
+    //     );
 
-        stable.transfer(msg.sender, stable.balanceOf(address(this)) * pTokenBalance / contractTokenBalance );
+    //     uint256 token1AmountAfter = IERC20(token2).balanceOf(msg.sender) - token1AmountBefore;
+    //     uint256 token2AmountAfter = IERC20(token2).balanceOf(msg.sender) - token2AmountBefore;
+    //     tokens[0] = token2;
+    //     tokens[1] = token1;
+
+    //     IRouter(router2).swapExactTokensForTokens(
+    //         token2AmountAfter,
+    //         1,
+    //         tokens,
+    //         msg.sender,
+    //         block.timestamp + 300
+    //     );
+
+    //     require(token1AmountAfter > token1AmountBefore, "Arbitrage: Unprofitable trade!");
+    //     profitFromArbitrage = token1AmountAfter - token1AmountBefore;
+    // }
+
+    // percnet - for know how many percent owner wantto receive
+    function profitDistribution(uint256 percent) external onlyOwner { 
+        require(profitFromArbitrage > 0, "Distribution: Not enough profit!");
+        stable.transfer(owner(), profitFromArbitrage * percent / 100);
+        profitFromArbitrage = 0;
     }
+
+
 }
--- test/Pool.test.js
+++ test/Pool.test.js
@@ -15,39 +15,67 @@ describe("Lock", function () {
       const PoolToken = await ethers.getContractFactory("PoolToken");
       const Stable = await ethers.getContractFactory("Stable");
       const pToken = await PoolToken.deploy();
-      const stable = await Stable.deploy();
-      const stableAddress = stable.address;
+      const stableToken = await Stable.deploy();
+      const stableAddress = stableToken.address;
       const tokenAddress = pToken.address;
-      const pool = await Pool.deploy(stable.address, tokenAddress);
+      const pool = await Pool.deploy(stableToken.address, tokenAddress);
       const token = await ethers.getContractAt("PoolToken", tokenAddress);
+      const stable = await ethers.getContractAt("Stable", stableAddress);
 
-      return { pool, token, tokenAddress, stableAddress, owner, caller, otherAccount };
+      return { pool, token, stable, tokenAddress, stableAddress, owner, caller, otherAccount };
   }
 
   describe("Initialization: ", function () {
     it("Should init with correct args: ", async function () {
-        const { pool, token, tokenAddress, stableAddress, owner, caller, otherAccount } = await loadFixture(deployPoolFixture);
-        // await pool(stableAddress, tokenAddress);
-        expect(await pool.stable()).is.equal(stableAddress);
-        expect(await pool.pToken()).is.equal(tokenAddress);
+      const { pool, token, tokenAddress, stableAddress, owner, caller, otherAccount } = await loadFixture(deployPoolFixture);
+      expect(await pool.stable()).is.equal(stableAddress);
+      expect(await pool.pToken()).is.equal(tokenAddress);
     });
   });
 
   describe("Deposit requires: ", function () {
     it("Should reverted with 'Amount not in range' ", async function () {
-        const { pool, token, tokenAddress, stableAddress, owner, caller, otherAccount } = await loadFixture(deployPoolFixture);
-        await expect(pool.deposit(10, stableAddress))
-                .to
-                .be
-                .revertedWith("Amount not in range");        
+      const { pool, token, tokenAddress, stableAddress, owner, caller, otherAccount } = await loadFixture(deployPoolFixture);
+      await expect(pool.deposit(10, stableAddress))
+              .to
+              .be
+              .revertedWith("Amount not in range");        
     });
+
+    it("Should reverted with 'Only stable' ", async function () {
+      const { pool, token, stable, tokenAddress, stableAddress, owner, caller, otherAccount } = await loadFixture(deployPoolFixture);
+      await expect(pool.deposit(200, tokenAddress))
+              .to
+              .be
+              .revertedWith("Only stable");        
+    });
+
+    it("Should reverted with 'Not enough balance' ", async function () {
+      const { pool, token, stable, tokenAddress, stableAddress, owner, caller, otherAccount } = await loadFixture(deployPoolFixture);
+      await expect(pool.deposit(200, stableAddress))
+              .to
+              .be
+              .revertedWith("Not enough balance");        
+    });
+
+    it("Should reverted with 'Not enough allowance' ", async function () {
+      const { pool, token, stable, tokenAddress, stableAddress, owner, caller, otherAccount } = await loadFixture(deployPoolFixture);
+      await stable.mint(caller.address, 1000);
+      await stable.approve(pool.address, 200);
+      await expect(pool.deposit(200, stableAddress))
+              .to
+              .be
+              .revertedWith("Not enough allowance");        
+    });
+
   });
 
-  describe("Deposit: ", function () {
-    it("Should deposit with correc args: ", async function () {
-        const { pool, token, tokenAddress, stableAddress, owner, caller, otherAccount } = await loadFixture(deployPoolFixture);
-        await caller
-  });
+  // describe("Deposit: ", function () {
+  //   it("Should deposit with correc args: ", async function () {
+  //       const { pool, token, tokenAddress, stableAddress, owner, caller, otherAccount } = await loadFixture(deployPoolFixture);
+  //       await caller
+  //   });
+  // });
 
 
 
